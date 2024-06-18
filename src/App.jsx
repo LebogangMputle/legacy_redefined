cfe8e99796a09dc7190cec54742296cd1e19c4a8
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-18 16:23:10 +0200

    Created Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
+import CourseLayout from "./components/CourseLayout";
+import CourseDetails from "./components/CourseDetails";
+import Integration from "./components/Integration";
+import Features from "./components/Features";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />}>
+            <Route index element={<Home />} />
+            <Route path="course-layout" element={<CourseLayout />} />
+            <Route path="course-detail" element={<CourseDetails />} />
+            <Route path="integration" element={<Integration />} />
+            <Route path="features" element={<Features />} />
+          </Route>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CourseLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CourseLayout = () => {
+  return (
+    <div>
+      <h1>Course Layout Page</h1>
+    </div>
+  );
+};
+
+export default CourseLayout;
