6cebd7aaa9ab70f67de91f068cb57b10b06e1010
Kitso Mogale <kitsomogale19@gmail.com>
2024-06-24 10:11:11 +0200

    setup about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,17 +4,17 @@ const About = () => {
   return (
     <div id='AboutUs'>
       <h1>About us</h1>
-       <div id='WhoWeAre'>
+       <div id='Who_We_Are'>
         <h2>Who We Are</h2>
 
        </div>
 
-       <div id='OurVision'>
+       <div id='Our_Vision'>
         <h2>Our Vision</h2>
 
        </div>
        
-       <div id='OurTeam'>
+       <div id='Our_Team'>
         <h2>Our Team</h2>
 
        </div>
