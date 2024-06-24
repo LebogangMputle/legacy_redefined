63bd1e992ef44cb3db1a43f605b3a84b60574366
LebogangMputle <lebozikalala@gmail.com>
2024-06-24 14:22:42 +0200

    Accepted merge from kutlwano

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,30 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "./Header";
-<<<<<<< HEAD
-import Courses from "../../pages/Courses";
+import Courses from "../../pages/ContactUs";
 import AboutUs from "../../pages/AboutUs";
 import Insights from "./Insights";
 
-=======
-import Courses from "../../pages/Courses/Courses";
-import About from "../../pages/AboutUs";
-// import './LandingPage.css'
->>>>>>> 7bfbbc3f29b323d0efa93057427af71b99c98a5a
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <Header />
-<<<<<<< HEAD
-      <section className="hero">
-
-        
-      </section>
-      <Courses />
-      <AboutUs />
-      <Insights/>
-=======
       <img
         src="https://media.licdn.com/dms/image/C4D0BAQHyCvbAh6do1g/company-logo_200_200/0/1669926955056?e=1727308800&v=beta&t=89bPiTA-CwAzAP3YjkakrCB1u4OZvykuEdZZUl5BCgc"
         alt="Courses Logo"
@@ -36,8 +21,8 @@ const LandingPage = () => {
         <p>Your distinguished business process and risk experts</p>
       </div>
       <Courses />
-      <About />
->>>>>>> 7bfbbc3f29b323d0efa93057427af71b99c98a5a
+      <AboutUs/>
+      <Insights />
     </div>
   );
 };
