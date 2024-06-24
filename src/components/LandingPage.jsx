cb887b48499fb6adcba3b46f31566badbe21f15b
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-24 14:41:12 +0200

    updated files

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "./Header";
-<<<<<<< HEAD
-import Courses from "../../pages/Courses";
 import AboutUs from "../../pages/AboutUs";
 import Insights from "./Insights";
 
-=======
 import Courses from "../../pages/Courses/Courses";
 import About from "../../pages/AboutUs";
 // import './LandingPage.css'
->>>>>>> 7bfbbc3f29b323d0efa93057427af71b99c98a5a
+
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <Header />
-<<<<<<< HEAD
-      <section className="hero">
 
-        
+      <section className="hero">
       </section>
-      <Courses />
-      <AboutUs />
-      <Insights/>
-=======
       <img
         src="https://media.licdn.com/dms/image/C4D0BAQHyCvbAh6do1g/company-logo_200_200/0/1669926955056?e=1727308800&v=beta&t=89bPiTA-CwAzAP3YjkakrCB1u4OZvykuEdZZUl5BCgc"
         alt="Courses Logo"
@@ -37,7 +28,9 @@ const LandingPage = () => {
       </div>
       <Courses />
       <About />
->>>>>>> 7bfbbc3f29b323d0efa93057427af71b99c98a5a
+      <Insights/>
+
+
     </div>
   );
 };
