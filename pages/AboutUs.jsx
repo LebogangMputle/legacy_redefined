de5f51b8914859a2b567db37d8c561c47fdb88af
Kitso Mogale <kitsomogale19@gmail.com>
2024-06-24 10:45:57 +0200

    cssAbout

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -1,21 +1,36 @@
 import React from "react";
+import './About.css'
 
 const About = () => {
   return (
     <div id='about'>
       <h1>About us</h1>
        <div id='Who_We_Are'>
-        <h2>Who We Are</h2>
+        <div id='div_wwa'>
+        <h3>Who We Are</h3>
+          <p id='p1'>
+            We are an Small Medium Enterprise (SME) consulting house and insights-led through strategic research.
+            We follow a risk-based approach which is key as adequate management of risk enforces sustainability within SMEs.
+             <br/>
+             <br/>
+            Our services:
+            Design, facilitate and monitoring of SME business process and risk management development programs
+            Strategic consulting and coaching
+            SME training on key business concepts
+            Digital marketing services and analytics through our supporting agency Thee Anchor
 
+          </p>
+
+          </div>
        </div>
 
        <div id='Our_Vision'>
-        <h2>Our Vision</h2>
+        <h3>Our Vision</h3>
 
        </div>
        
        <div id='Our_Team'>
-        <h2>Our Team</h2>
+        <h3>Our Team</h3>
 
        </div>
 
