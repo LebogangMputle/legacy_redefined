8995da4d572b5289f5adec957e983534f2f5685d
Kitso Mogale <kitsomogale19@gmail.com>
2024-06-24 11:32:15 +0200

    Our Team

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -5,10 +5,11 @@ const About = () => {
   return (
     <div id='about'>
       <h1>About us</h1>
+       <div id='div_wwa'>
        <div id='Who_We_Are'>
-        <div id='div_wwa'>
-        <h3>Who We Are</h3>
-          <p id='p1'>
+       
+        <h2>Who We Are</h2>
+          <p className='p1'>
             We are an Small Medium Enterprise (SME) consulting house and insights-led through strategic research.
             We follow a risk-based approach which is key as adequate management of risk enforces sustainability within SMEs.
              <br/>
@@ -21,19 +22,45 @@ const About = () => {
 
           </p>
 
-          </div>
+          
        </div>
 
        <div id='Our_Vision'>
-        <h3>Our Vision</h3>
+        <h2>Our Vision</h2>
+        <p className='p1'>
+        We aim to witness enduring businesses supported by efficient processes, innovative Information Technology solutions, and timeless brands.
+        We motivate organisations to integrate strong processes, backed by resilient Information Technology solutions and infused with creativity to build brands that guarantee top-of-mind awareness
+
+
+        </p>
 
        </div>
        
        <div id='Our_Team'>
-        <h3>Our Team</h3>
+        <h2>Our Team</h2>
+          <div id='divSlide'>
+               <div className="members">
+                  <p className="pNames">Lesego Khoele (CIA),Mphil</p>
+                  <p className="pRole">Md and Founder<br/> SMME Business Developer</p>
+               </div>
 
+               <div className="members">
+                <p className="pNames">Karabo Kgomanyane</p>
+                <p className="pRole">Data-Led Marketing Specialist <br/>Thee Anchor MD & Founder</p>
+               </div>
+
+               <div className="members">
+                <p className="pNames">Mphoma Putsoane </p>
+                <p className="pRole">Business Accounting<br/>Development Specialist</p>
+               </div>
+
+               <div className="members">
+                <p className="pNames">Boipelo Maringa </p>
+                <p className="pRole">Business and Personal <br/>Finance Specialist</p>
+               </div>
+          </div>
        </div>
-
+     </div>
     </div>
   );
 };
