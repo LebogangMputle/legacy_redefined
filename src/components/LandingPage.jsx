363c85b50fe597babf48510f46a57795f44f411c
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-26 10:16:09 +0200

    updated files

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -13,13 +13,6 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <Header />
-
-      <img
-        src="https://media.licdn.com/dms/image/C4D0BAQHyCvbAh6do1g/company-logo_200_200/0/1669926955056?e=1727308800&v=beta&t=89bPiTA-CwAzAP3YjkakrCB1u4OZvykuEdZZUl5BCgc"
-        alt="Courses Logo"
-        className="background-image animated fadeIn" // Apply animation class
-      />
-
       <div className="content">
         <h1 className="H1">Legacy Redefined</h1>
         <p>Redefining business processes and risk</p>
